b9085fd727f1cf22161b4448def0958b737afd7f
michau <michal.radziukiewicz@gmail.com>
2024-12-08 15:12:44 +0100

    Database config changes and readme update

--- KanTask/settings.py
+++ KanTask/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'KanTask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoDB',
+        'USER': 'admin',
+        'PASSWORD': 'KanTask123!',
+        'HOST': '31.183.16.15',
+        'PORT': '3306',
     }
 }
 
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ venv\Scripts\activate
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py runserver
+
+
+#Dodatkowo wymagane połączenie do bazy danych 31.183.16.15, zaptaj o dostep jesli nie masz ;)
